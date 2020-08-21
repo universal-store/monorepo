a9a497f444bdfdfad26a6606a3886a322c07f506
Christopher Jeffrey Obando <chrisjobando@gmail.com>
2020-08-21 12:54:02 -0400

    feat: added secret env file

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -7,13 +7,18 @@ UNSET='\033[0m' # No Color
 
 STEP=0
 
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+fi
+
 e() {
   STEP=$((STEP + 1))
   echo "${FG_BLUE}$((STEP)). $1${UNSET}"
 }
 
 e "Get Schema from Hasura"
-cd "$dir"/server && gq http://localhost:8080/v1/graphql --introspect > schema.graphql
+cd "$dir"/server && gq $API_URL --introspect > schema.graphql
 
 e "Run graphql-codegen"
 cd "$dir" && graphql-codegen --config codegen.yml

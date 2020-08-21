84e40205919d186169d5a7efd213c485f1b7b371
Christopher Jeffrey Obando <chrisjobando@gmail.com>
2020-08-21 08:49:07 -0400

    feat: added postinstall script

---
+++ scripts/postinstall.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+dir=$(pwd)
+
+FG_BLUE='\033[1;34m'
+UNSET='\033[0m' # No Color
+
+STEP=0
+
+e() {
+  STEP=$((STEP + 1))
+  echo "${FG_BLUE}$((STEP)). $1${UNSET}"
+}
+
+e "Generate @prisma/client"
+cd "$dir"/database && yarn generate:datamodel
+
+e "Run graphql-codegen"
+cd "$dir" && graphql-codegen --config codegen.yml

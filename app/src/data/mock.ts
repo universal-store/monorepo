3c09df5c9317d0ba9a4b89d9069ce4acc572118e
Marie Ow <marieow35@gmail.com>
2020-08-31 19:34:15 -0400

    added id to mock data

--- app/src/data/mock.ts
+++ app/src/data/mock.ts
@@ -3,23 +3,33 @@
 import console = require('console');
 
 export interface MockBarcode {
-  shortName: String;
-  longName: String;
-  size: String;
+  id: string;
+  shortName: string;
+  longName: string;
+  size: string;
   price: number;
-  description: String;
+  description: string;
 }
 
 let data: MockBarcode[] = [
   {
+    id: '123456',
     shortName: 'shortNameExample',
     longName: 'longNameExample',
     size: 'sizeExample',
     price: 10.0,
     description: 'descriptionExample',
   },
-  { shortName: 'Water', longName: 'Dasani Water Bottle', size: '16 oz', price: 2.0, description: 'Super clear water' },
   {
+    id: '234289',
+    shortName: 'Water',
+    longName: 'Dasani Water Bottle',
+    size: '16 oz',
+    price: 2.0,
+    description: 'Super clear water',
+  },
+  {
+    id: '778653',
     shortName: 'Bread',
     longName: 'Nature Valley Honey Whole Wheat Bread',
     size: '12 Slices',

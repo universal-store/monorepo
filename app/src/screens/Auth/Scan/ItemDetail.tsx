f15909ef68aff6b23f71d0822d7196ec31134795
Christopher Jeffrey Obando <chrisjobando@gmail.com>
2020-09-02 12:59:41 -0400

    feat: added heart icons

--- app/src/screens/Auth/Scan/ItemDetail.tsx
+++ app/src/screens/Auth/Scan/ItemDetail.tsx
@@ -5,10 +5,21 @@ import React from 'react';
 // Custom Components
 import styled from 'styled-components/native';
 
-const ItemDetailContainer = styled.View`
-  flex: 1;
+// Icons
+// import { HeartIcon } from '&icons';
+
+// Components
+import { FullScreen } from '&components';
+
+const ItemDetailContainer = styled(FullScreen)`
   background-color: ${({ theme }) => theme.colors.purple[3]};
 `;
+
+// const HeartIconContainer = styled.View`
+//   display: flex;
+//   width: 24px;
+//   height: 24px;
+// `;
 
 export const ItemDetail = () => {
   return <ItemDetailContainer />;

bf3ae16dd72a1a1d9e1b06c1d43553a57f06774c
Christopher Jeffrey Obando <chrisjobando@gmail.com>
2020-09-04 07:34:38 -0400

    fix: re-formatting styled components

--- app/src/screens/Auth/Scan/ItemDetail.tsx
+++ app/src/screens/Auth/Scan/ItemDetail.tsx
@@ -3,20 +3,23 @@
 import React from 'react';
 
 // Components
-import { FullScreen, ItemDetailsComponents } from '&components';
-
-// Custom Components
-import styled from 'styled-components/native';
-
-const ItemDetailBackground = styled.View`
-  flex: 1;
-  background-color: ${({ theme }) => theme.colors.purple[3]};
-`;
 
 export const ItemDetail = () => {
   return (
-    <FullScreen>
-      <ItemDetailsComponents></ItemDetailsComponents>
-    </FullScreen>
+    <ItemDetailBackground>
+      <ExitButton></ExitButton>
+      <ViewCartButton></ViewCartButton>
+      <ItemDetailContainer>
+        <ItemName>Gatorade Thirst Quencher Sports Drink, Orange</ItemName>
+        <ItemSize>28 oz Bottle</ItemSize>
+        <ItemPrice>$3.19</ItemPrice>
+        <ProductDetailsHeader>Product Details</ProductDetailsHeader>
+        <ProductDetailsText>
+          When you sweat, you lose more than water. You also lose critical electrolytes, like sodium and potassium,
+          which help the brain communicate with muscles and regulate fluid balance throughout the body. Significant
+          losses in fluid and electrolytes can negatively impact performance...
+        </ProductDetailsText>
+      </ItemDetailContainer>
+    </ItemDetailBackground>
   );
 };

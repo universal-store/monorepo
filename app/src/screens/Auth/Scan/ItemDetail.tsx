bfa0366b3198293ceaef63d6c1d156b98a87a7b1
Christopher Jeffrey Obando <chrisjobando@gmail.com>
2020-09-06 16:20:08 -0400

    feat: added back bottom sheet, configured it correctly and added header to slide up/down

--- app/src/screens/Auth/Scan/ItemDetail.tsx
+++ app/src/screens/Auth/Scan/ItemDetail.tsx
@@ -2,11 +2,17 @@
 
 import React, { useState } from 'react';
 
+// Libraries
+import Animated from 'react-native-reanimated';
+import BottomSheet from 'reanimated-bottom-sheet';
+
+// Components
 import {
+  AddCartButton,
   AddCartButtonContainer,
   AddCartButtonText,
   FuturaBoldLarge as ItemText,
-  ItemAdditionalInfoScroll,
+  isiPhoneX,
   ItemDetailContainer,
   ItemDetailFavoriteButton,
   ItemDetailHeaderButton,
@@ -14,11 +20,14 @@ import {
   ItemDetailImage,
   ItemDetailImageContainer,
   ItemDetailModalContainer,
+  ItemDetailModalHeader,
+  ItemDetailModalHeaderTab,
   ItemNameText,
   ItemSizeText,
   ItemSubDetailRow,
   ProductDetailsHeaderText,
   ProductDetailsText,
+  screenHeight,
 } from '&components';
 
 // Iconography
@@ -31,17 +40,58 @@ import { GetStoreItemDocument, useGetStoreItemQuery, useUpdateStoreItemPurchaseM
 import { AuthStackParams } from '&navigation';
 import { StackScreenProps } from '@react-navigation/stack';
 
+// Constants
+const largeModalHeight = screenHeight - (isiPhoneX ? 122 : 92);
+const smallModalHeight = screenHeight - (isiPhoneX ? 402 : 372);
+
+const { eq, Value } = Animated;
+
 type ItemDetailProps = StackScreenProps<AuthStackParams, 'ItemDetail'>;
 
 export const ItemDetail = ({ route, navigation }: ItemDetailProps) => {
   const { barcodeId } = route.params;
 
+  // TODO: Replace with User Favorites Mutation
   const [favorite, setFavorite] = useState<boolean>(false);
 
   const { data } = useGetStoreItemQuery({ variables: { barcodeId } });
   const itemData = data?.StoreItem_by_pk;
 
   const [updatePurchaseMutation] = useUpdateStoreItemPurchaseMutation();
+
+  const modalHeight = new Value(1);
+
+  const renderHeader = () => (
+    <ItemDetailModalHeader>
+      <ItemDetailModalHeaderTab />
+    </ItemDetailModalHeader>
+  );
+
+  const renderContent = () => (
+    <>
+      {itemData && (
+        <ItemDetailModalContainer>
+          <ItemSubDetailRow>
+            <ItemNameText numberOfLines={2}>{itemData.longName}</ItemNameText>
+
+            <ItemDetailFavoriteButton onPress={() => setFavorite(!favorite)}>
+              {favorite ? <HeartIconOn /> : <HeartIconOff />}
+            </ItemDetailFavoriteButton>
+          </ItemSubDetailRow>
+
+          <ItemSubDetailRow>
+            <ItemSizeText numberOfLines={1}>{itemData.quantity}</ItemSizeText>
+            <ItemText>{itemData.price}</ItemText>
+          </ItemSubDetailRow>
+
+          <ProductDetailsHeaderText>Product Details</ProductDetailsHeaderText>
+          <ProductDetailsText numberOfLines={eq(modalHeight, new Value(1)) ? 5 : 10}>
+            {itemData.description}
+          </ProductDetailsText>
+        </ItemDetailModalContainer>
+      )}
+    </>
+  );
 
   return (
     <ItemDetailContainer>
@@ -65,27 +115,19 @@ export const ItemDetail = ({ route, navigation }: ItemDetailProps) => {
         )}
       </ItemDetailImageContainer>
 
+      <BottomSheet
+        initialSnap={1}
+        callbackNode={modalHeight}
+        renderHeader={renderHeader}
+        renderContent={renderContent}
+        enabledBottomInitialAnimation
+        enabledContentGestureInteraction={false}
+        snapPoints={[largeModalHeight, smallModalHeight]}
+      />
+
       {itemData && (
-        <ItemDetailModalContainer>
-          <ItemSubDetailRow>
-            <ItemNameText numberOfLines={2}>{itemData.longName}</ItemNameText>
-
-            <ItemDetailFavoriteButton onPress={() => setFavorite(!favorite)}>
-              {favorite ? <HeartIconOn /> : <HeartIconOff />}
-            </ItemDetailFavoriteButton>
-          </ItemSubDetailRow>
-
-          <ItemAdditionalInfoScroll bounces={false} showsVerticalScrollIndicator={false}>
-            <ItemSubDetailRow>
-              <ItemSizeText numberOfLines={1}>{itemData.quantity}</ItemSizeText>
-              <ItemText>{itemData.price}</ItemText>
-            </ItemSubDetailRow>
-
-            <ProductDetailsHeaderText>Product Details</ProductDetailsHeaderText>
-            <ProductDetailsText>{itemData.description}</ProductDetailsText>
-          </ItemAdditionalInfoScroll>
-
-          <AddCartButtonContainer
+        <AddCartButtonContainer>
+          <AddCartButton
             added={itemData.purchased}
             onPress={() =>
               void updatePurchaseMutation({
@@ -105,8 +147,8 @@ export const ItemDetail = ({ route, navigation }: ItemDetailProps) => {
             <AddCartButtonText added={itemData.purchased}>
               {itemData.purchased ? 'Added!' : 'Add to Cart'}
             </AddCartButtonText>
-          </AddCartButtonContainer>
-        </ItemDetailModalContainer>
+          </AddCartButton>
+        </AddCartButtonContainer>
       )}
     </ItemDetailContainer>
   );

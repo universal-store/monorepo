1e92e024647eccc27e7b9839a8070bcb114542ed
Christopher Jeffrey Obando <35117309+chrisjobando@users.noreply.github.com>
2020-09-14 12:07:04 -0400

    Feat: added onboarding icons (#59)

---
+++ app/src/icons/EmailIcon.tsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+export const EmailIcon = () => {
+  return (
+    <Svg width={16} height={16} fill="none">
+      <Path
+        d="M15.224 4.67L8 0 .776 4.67C.312 4.958 0 5.48 0 6.082v8.264C0 15.257.72 16 1.6 16h12.8c.88 0 1.6-.744 1.6-1.653V6.083c0-.604-.312-1.124-.776-1.414zm-.824 9.677H1.6V7.736L8 11.868l6.4-4.132v6.611zM8 10.215L1.6 6.083 8 1.95l6.4 4.133L8 10.215z"
+        fill="#333"
+      />
+    </Svg>
+  );
+};

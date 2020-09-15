0c73c342efa920acb0962207d4687140ee85f397
unknown <samehhamadeh@gmail.com>
2020-09-14 21:28:00 -0400

    Add logo box and text to splash screen

--- app/src/components/Landing/Styled.ts
+++ app/src/components/Landing/Styled.ts
@@ -1,3 +1,28 @@
 /** @format */
 
-import 'styled-components/native';
+import styled from 'styled-components/native';
+import { FuturaBoldMedium } from '../Text';
+
+export const SplashLogoBox = styled.View`
+  display: flex;
+  width: 200px;
+  height: 200px;
+  background-color: ${({ theme }) => theme.colors.purple[1]};
+  margin: 112px auto 0;
+  border-radius: 30px;
+`;
+
+export const SplashTitleText = styled(FuturaBoldMedium)`
+  margin-top: 29px;
+  font-size: 21px;
+  line-height: 28px;
+  text-align: center;
+`;
+
+export const SplashSubtitleText = styled(FuturaBoldMedium)`
+  margin-top: 5px;
+  font-size: 15px;
+  color: ${({ theme }) => theme.colors.gray[3]};
+  line-height: 20px;
+  text-align: center;
+`;

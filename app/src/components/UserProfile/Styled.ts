7c6cc5be1f2558bba9349028a8b62ffd5a9d9785
Daniel Keehn <danielkeehn@bellsouth.net>
2020-10-13 04:31:30 -0400

    Added components and put elements on screen for user screen

--- app/src/components/UserProfile/Styled.ts
+++ app/src/components/UserProfile/Styled.ts
@@ -4,5 +4,60 @@ import styled from 'styled-components/native';
 import { Animated } from 'react-native';
 
 // Components
-import { FullScreenCenter, isiPhoneX, RowView, screenWidth } from '../Views';
-import { HeaderLargeText, HeaderSmallText, TextMedium, TextMedium2 } from '../Text';
+import { FullScreenWhite, RowView } from '../Views';
+import { HeaderLargeText, HeaderSmallText, TextSmall, TextSmall2 } from '../Text';
+
+export const UserProfileMainContainer = styled(FullScreenWhite)`
+  flex: 1;
+  width: 100%;
+  display: flex;
+  padding: 24px;
+`;
+
+export const UserProfileHeaderText = styled(HeaderLargeText)`
+  color: ${({ theme }) => theme.colors.gray[1]};
+`;
+
+export const UserProfileSubHeaderText = styled(HeaderSmallText)`
+  margin-bottom: 6px;
+  color: ${({ theme }) => theme.colors.gray[1]};
+`;
+
+export const UserProfileEmailText = styled(TextSmall)`
+  margin-bottom: 4px;
+  color: ${({ theme }) => theme.colors.gray[1]};
+`;
+
+export const UserProfilePaymentInfoText = styled(TextSmall2)`
+  margin-bottom: 4px;
+  color: ${({ theme }) => theme.colors.gray[1]};
+`;
+
+export const UserProfileProfilePictureContainer = styled.View`
+  width: 64px;
+  height: 64px;
+  margin-right: 24px;
+`;
+
+export const UserProfileEmailContainer = styled.View`
+  margin-bottom: 16px;
+  border-bottom-width: 1px;
+  border-bottom-color: ${({ theme }) => theme.colors.gray[5]};
+`;
+
+export const UserProfilePaymentInfoContainer = styled.View`
+  margin-bottom: 16px;
+`;
+
+export const UserProfileCheckMarkLogoContainer = styled.View`
+  width: 24px;
+  height: 24px;
+  margin-left: auto;
+  margin-right: 4px;
+`;
+
+export const UserProfileHeaderRow = styled(RowView)`
+  margin-bottom: 24px;
+`;
+
+export const UserPaymentInfoNameRow = styled(RowView)``;
--- app/src/screens/Auth/User/UserProfile.tsx
+++ app/src/screens/Auth/User/UserProfile.tsx
@@ -4,7 +4,22 @@ import React, { useEffect, useState } from 'react';
 import { Linking } from 'react-native';
 
 // Components
-import { TestButtonText } from '&components';
+import {
+  UserProfileMainContainer,
+  UserProfileHeaderRow,
+  UserProfileProfilePictureContainer,
+  UserProfileHeaderText,
+  UserProfileSubHeaderText,
+  UserProfileEmailContainer,
+  UserProfileEmailText,
+  UserProfilePaymentInfoContainer,
+  UserProfilePaymentInfoText,
+  UserPaymentInfoNameRow,
+  UserProfileCheckMarkLogoContainer,
+} from '&components';
+
+// Iconography
+import { PersonIcon } from '&icons';
 
 // Navigation
 import { AuthStackParams } from '&navigation';
@@ -14,5 +29,28 @@ import { StackScreenProps } from '@react-navigation/stack';
 type UserProfileProps = StackScreenProps<AuthStackParams, 'UserProfile'>;
 
 export const UserProfileScreen = ({ navigation }: UserProfileProps) => {
-  return <TestButtonText>Hello World</TestButtonText>;
+  return (
+    <UserProfileMainContainer>
+      <UserProfileHeaderRow>
+        <UserProfileProfilePictureContainer></UserProfileProfilePictureContainer>
+        <UserProfileHeaderText>Hi, Daniel Keehn</UserProfileHeaderText>
+      </UserProfileHeaderRow>
+      <UserProfileSubHeaderText>Email Address</UserProfileSubHeaderText>
+      <UserProfileEmailContainer>
+        <UserProfileEmailText>dkeehn6@gatech.edu</UserProfileEmailText>
+      </UserProfileEmailContainer>
+      <UserProfileSubHeaderText>Payment Method</UserProfileSubHeaderText>
+      <UserProfilePaymentInfoContainer>
+        <UserProfilePaymentInfoText>VISA Debit (1834)</UserProfilePaymentInfoText>
+        <UserPaymentInfoNameRow>
+          <UserProfilePaymentInfoText>Daniel Keehn</UserProfilePaymentInfoText>
+          <UserProfileCheckMarkLogoContainer>
+            <PersonIcon></PersonIcon>
+          </UserProfileCheckMarkLogoContainer>
+        </UserPaymentInfoNameRow>
+        <UserProfilePaymentInfoText>Exp: 07/24</UserProfilePaymentInfoText>
+      </UserProfilePaymentInfoContainer>
+      <UserProfileSubHeaderText>My Orders</UserProfileSubHeaderText>
+    </UserProfileMainContainer>
+  );
 };

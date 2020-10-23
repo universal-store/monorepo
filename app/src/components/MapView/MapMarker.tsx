72d30637c83a434ebbeaa382dec0f9423c87f7c3
Daniel Keehn <danielkeehn@bellsouth.net>
2020-10-23 02:21:11 -0400

    Map Marker Icon Shows Up On Map

---
+++ app/src/components/MapView/MapMarker.tsx
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from 'react';
+
+// Components
+import {} from './Styled';
+
+import MapView, { Marker } from 'react-native-maps';
+
+export const MapMarker = () => {};
